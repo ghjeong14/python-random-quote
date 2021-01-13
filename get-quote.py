83675576a31cf98abb283a06d9f4619130a9d5eb
정기혁 <geehyuk@jeong-gihyeog-ui-MacBook-Pro.local>
2021-01-13 21:04:13 +0900

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def hellofunction():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+ 
 
-  #print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
   hellofunction()
