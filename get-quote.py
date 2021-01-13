d8dfd741555c1ff79e6b5947f5ff40fea34b8697
ghjeong14 <gh.jeong84@gmail.com>
2021-01-13 21:53:43 +0900

    I made a bot which reads lines and deletes specific line

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,42 @@
 def hellofunction():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
+  """
+  f = open("quotes.txt", "a")
+  f.write("\nFirst Line\n")
+  f.write("Second Line")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt", "r")
   quotes = f.readlines()
   f.close()
  
 
-  print(quotes[13])
+  print(quotes[13], end='')
+  print(quotes[0], end='')
+  print(quotes[19], end='')
+  print(quotes[20])
+  """
+  with open("quotes.txt", "r") as f:
+    lines = f.readlines()                     #open txt and close
+  with open("quotes.txt", "w") as f:
+    for line in lines:
+      if line.strip("\n") != "First Line":
+        f.write(line)
+  with open("quotes.txt", "r") as f:
+    lines = f.readlines() 
+  with open("quotes.txt", "w") as f:
+    for line in lines:
+      if line.strip("\n") != "Second Line":
+        f.write(line)
+  """with open("quotes.txt", "r") as f:
+    quotes = f.readlines()   
+
+  print(quotes[13], end='')
+  print(quotes[0], end='')
+  print(quotes[19], end='')
+  print(quotes[20])"""
+
+
+
 
 if __name__== "__main__":
   hellofunction()
